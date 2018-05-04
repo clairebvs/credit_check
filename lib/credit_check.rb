2f75638ccdbe9a07d72a90e6204ddf3f7902f0df
ClaireBvs <clairembeauvais@gmail.com>
2018-05-03 18:27:39 -0600

    creating array with digits instead of chars and testing double every other digit

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -2,11 +2,19 @@ class CreditCheck
   attr_reader :card_number
 
   def initialize(card_number)
-    @card_number = card_number
+    @card_number = card_number.to_i.digits
+    # binding.pry
   end
 
-  def convert_to_array
-    @card_number.chars
+  def double_every_other_digit
+    double_number = @card_number.each_with_index.map { |card_number, index|
+      if index.even?
+        card_number
+      else
+        card_number * 2
+      end
+    }
+    binding.pry
   end
 
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -11,20 +11,15 @@ class CreditCheckTest < Minitest::Test
     assert_instance_of CreditCheck, credit
   end
 
-  def test_it_has_a_card_number
+  def test_convert_string_to_reverse_array
     credit = CreditCheck.new("4929735477250543")
 
-    assert_equal "4929735477250543", credit.card_number
+    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], credit.card_number
   end
 
-  def test_card_number_can_be_converted_to_array
+  def test_can_double_every_other_digit
     credit = CreditCheck.new("4929735477250543")
 
-    assert_equal ["4", "9", "2", "9", "7", "3", "5", "4", "7", "7", "2", "5", "0", "5", "4", "3"], credit.convert_to_array
-  end
-
-  def test_can_reverse_card_number
-    skip
-    credit
+    assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], credit.double_every_other_digit 
   end
 end
